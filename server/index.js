e331c7bf077ac119f332d1d576abe16fa84d2c26
Ederson Davi <silva.edersonqueiroz@gmail.com>
2020-07-07 19:46:31 -0300

    First commit

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import path from "path";
+import socketIO from "socket.io";
+const app = express();
+
+const list = ["José", "Marcos", "Antônio"];
+
+// console.log(list);
+// setTimeout(() => {
+//   list.push("Pedro");
+//   console.log(list);
+// }, 3000);
+
+// app.get("/list", (req, res) => {
+//   res.send(list);
+// });
+
+app.use("/", express.static(path.resolve(__dirname, "..", "public")));
+
+const server = app.listen(5000, () => {
+  console.log("Server running port 5000");
+});
+
+const io = socketIO(server);
+
+io.on("connection", (socket) => {
+  console.log("New connectiom");
+});
